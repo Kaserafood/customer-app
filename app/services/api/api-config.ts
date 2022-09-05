240f6bddd4697f96dff1df10f53939c6cbcd13a1
Kasera <dev@kaserafood.com>
2022-09-04 22:07:31 -0500

    Update podfile and api-config

--- app/services/api/api-config.ts
+++ app/services/api/api-config.ts
@@ -4,8 +4,8 @@
 
 let API_URL = ""
 if (__DEV__) {
-  API_URL = "http://192.168.0.9:3000"
-  //API_URL = "https://kasera-customer-api-test.azurewebsites.net"
+//API_URL = "http://192.168.0.9:3000"
+  API_URL = "https://kasera-customer-api-test.azurewebsites.net"
 } else {
   API_URL = "https://kasera-customer-api.azurewebsites.net"
 }
