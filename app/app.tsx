dd2be5e827fc77d07873ba91f3a38777a447a745
Carlos Cunquero <dev@kaserafood.com>
2022-10-08 10:26:18 -0600

    add facebook sdk

--- app/app.tsx
+++ app/app.tsx
@@ -13,6 +13,7 @@ import React, { useEffect, useState } from "react"
 import { Settings } from "react-native-fbsdk-next"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { enableLatestRenderer } from "react-native-maps"
+import OneSignal from "react-native-onesignal"
 import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
 import { ToggleStorybook } from "../storybook/toggle-storybook"
 import { Loader, Messages } from "./components"
@@ -53,6 +54,8 @@ function App() {
     })()
     Settings.initializeSDK()
     Settings.setAdvertiserTrackingEnabled(true)
+    OneSignal.setAppId("c6f16d8c-f9d4-4d3b-8f25-a1b24ac2244a")
+
   }, [])
 
   // Before we show the app, we have to wait for our state to be ready.
